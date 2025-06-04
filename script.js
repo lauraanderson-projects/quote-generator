688c2bb3f9b48e17587abd53f1e5e91c7151c955
sphirebluela <laura.sphireblue@gmail.com>
2025-06-03 21:06:40 -0700

    adding javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+//Get quotes from api
+let apiQuotes = [];
+
+//Show new quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //check if Author: Firstname Lastname is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //console.log(error);
+  }
+}
+
+//On Load
+getQuotes();
